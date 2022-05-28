d477719c34d4032b1675cf484762d3caedcff0c6
KP <prasanth.25k@gmail.com>
2022-05-28 10:55:51 +0530

    formatting

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -7,26 +7,26 @@ require("packer").startup(
       "nvim-treesitter/nvim-treesitter",
       run = ":TSUpdate",
       config = function()
-          require("plugins.configs.treesitter")
+        require("plugins.configs.treesitter")
       end
     }
 
     use "williamboman/nvim-lsp-installer"
 
     use {
-        "neovim/nvim-lspconfig",
-        after = "nvim-lsp-installer",
-        config = function()
-            require("plugins.configs.lspconfig")
-        end
+      "neovim/nvim-lspconfig",
+      after = "nvim-lsp-installer",
+      config = function()
+        require("plugins.configs.lspconfig")
+      end
     }
 
     use({
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-           require("plugins.configs.null-ls")
-        end,
-        requires = { "nvim-lua/plenary.nvim" },
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function()
+        require("plugins.configs.null-ls")
+      end,
+      requires = { "nvim-lua/plenary.nvim" },
     })
 
     use {
@@ -37,21 +37,21 @@ require("packer").startup(
     use 'folke/tokyonight.nvim'
 
     use {
-        "nvim-neorg/neorg",
-        requires = "nvim-lua/plenary.nvim",
-        config = function()
-            require("plugins.configs.neorg")
-        end
+      "nvim-neorg/neorg",
+      requires = "nvim-lua/plenary.nvim",
+      config = function()
+        require("plugins.configs.neorg")
+      end
     }
 
     use {
       "nvim-neo-tree/neo-tree.nvim",
-        branch = "v2.x",
-        requires = {
-          "nvim-lua/plenary.nvim",
-          "kyazdani42/nvim-web-devicons",
-          "MunifTanjim/nui.nvim",
-        }
+      branch = "v2.x",
+      requires = {
+        "nvim-lua/plenary.nvim",
+        "kyazdani42/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+      }
     }
 
     use {
@@ -61,22 +61,20 @@ require("packer").startup(
         opt = true
       },
       config = function()
-          require("lualine").setup {
-              options = {
-                  theme = 'tokyonight'
-              }
+        require("lualine").setup {
+          options = {
+            theme = 'tokyonight'
           }
+        }
       end
     }
 
     use {
-        'karb94/neoscroll.nvim',
-        config = function()
-            require('neoscroll').setup()
-        end
+      'karb94/neoscroll.nvim',
+      config = function()
+        require('neoscroll').setup()
+      end
     }
-
-    -- use "Pocco81/AutoSave.nvim"
 
   end
 )
