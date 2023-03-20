db598d22ea299e7d43eb6bdefc8c7c2dc8472226
KP <prasanth.25k@gmail.com>
2023-03-20 14:46:57 +0530

    [nvim] update which key config

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -5,11 +5,6 @@ return {
 		dependencies = { {'nvim-lua/plenary.nvim'} },
 	},
 	{
-		"folke/which-key.nvim",
-		config = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-			require("which-key").setup({})
-		end,
+		"folke/which-key.nvim", lazy = true
 	}
 }
