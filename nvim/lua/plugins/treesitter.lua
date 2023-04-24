c46da5e8a1f2e22c8a556e2e396a41fced1953c5
KP <prasanth.25k@gmail.com>
2023-04-24 09:03:15 +0530

    [nvim] rename help parser to vim-doc

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
 				enable = true,
 				additional_vim_regex_highlighting = false
 			},
-			ensure_installed = { "lua", "python", "typescript", "vim", "zig" },
+			ensure_installed = { "lua", "python", "typescript", "vim", "vimdoc", "zig" },
 		},
 		config = function(_, opts)
 			require("nvim-treesitter.configs").setup(opts)
