30f0575fb9e1c3c3681a3b5b963355c60bd0e24d
KP <prasanth.25k@gmail.com>
2023-10-30 12:48:51 +0530

    [nvim] add telescope

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -10,5 +10,17 @@ return {
 			vim.cmd("colorscheme gruvbox-material")
 		end
 	},
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.4',
+		dependencies = {
+			'nvim-lua/plenary.nvim'
+		},
+		config = function()
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+		end
+	},
 	"github/copilot.vim",
 }
