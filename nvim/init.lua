816b9a025d5dc797ce085fc8dda055a723be671d
KP <prasanth.25k@gmail.com>
2023-10-30 10:46:00 +0530

    [nvim] add plugins to a separate file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,3 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
 -- set leader to <Space>
 vim.g.mapleader = " "
 
@@ -24,4 +11,17 @@ vim.opt.shiftwidth = 4	-- number of spaces used while indentation
 vim.opt.softtabstop = 4	-- number of spaces when editing text
 vim.opt.smarttab = true	-- enhances the behaviour of tab key based on context
 
-require("lazy").setup({}, opts)
+-- package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("plugins")
