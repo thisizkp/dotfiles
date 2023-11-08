b70bc2d19a4982e49d7441acc8eb2b678530c45f
KP <prasanth.25k@gmail.com>
2023-11-08 17:06:30 +0530

    [nvim] use gruvbox original & update plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,14 +1,11 @@
 return {
-	{
-		"sainnhe/gruvbox-material",
-		lazy = false,
-		priority = 1000,
+	{ 
+		"ellisonleao/gruvbox.nvim", 
+		priority = 1000 , 
 		config = function()
-			vim.g.gruvbox_material_background = "hard"
-			vim.g.gruvbox_material_better_performance = 1
-			vim.g.gruvbox_material_enable_italic = 1
-			vim.cmd("colorscheme gruvbox-material")
-		end
+			require('gruvbox').setup()
+			vim.cmd('colorscheme gruvbox')
+		end 
 	},
 	{
 		'numToStr/Comment.nvim',
