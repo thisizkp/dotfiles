9971aa8b1fc1195234a811c6401000d4aebfe937
KP <prasanth.25k@gmail.com>
2023-11-19 15:36:13 +0530

    [nvim] enable auto format

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,10 +10,10 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- indent
-vim.opt.tabstop = 4	-- number of spaces a tab character will occupy
-vim.opt.shiftwidth = 4	-- number of spaces used while indentation 
-vim.opt.softtabstop = 4	-- number of spaces when editing text
-vim.opt.smarttab = true	-- enhances the behaviour of tab key based on context
+vim.opt.tabstop = 4     -- number of spaces a tab character will occupy
+vim.opt.shiftwidth = 4  -- number of spaces used while indentation
+vim.opt.softtabstop = 4 -- number of spaces when editing text
+vim.opt.smarttab = true -- enhances the behaviour of tab key based on context
 
 -- background
 vim.opt.background = "light"
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -100,6 +100,7 @@ return {
 				-- see :help lsp-zero-keybindings
 				-- to learn the available actions
 				lsp_zero.default_keymaps({ buffer = bufnr })
+				lsp_zero.buffer_autoformat()
 			end)
 
 			require('mason-lspconfig').setup({
