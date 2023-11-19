9d32d7a34a25bef83a1130537dd75b07e1da84a6
KP <prasanth.25k@gmail.com>
2023-11-19 11:33:19 +0530

    [nvim] add lsp

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -42,4 +42,43 @@ return {
   end,
 },
 	"github/copilot.vim",
+	"neovim/nvim-lspconfig",
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup()
+			
+			local handlers = {
+           -- The first entry (without a key) will be the default handler
+           -- and will be called for each installed server that doesn't have
+           -- a dedicated handler.
+           function (server_name) -- default handler (optional)
+               require("lspconfig")[server_name].setup {}
+           end,
+           -- Next, you can provide targeted overrides for specific servers.
+           -- ["rust_analyzer"] = function ()
+           --     require("rust-tools").setup {}
+           -- end,
+           -- ["lua_ls"] = function ()
+           --     local lspconfig = require("lspconfig")
+           --     lspconfig.lua_ls.setup {
+           --         settings = {
+           --             Lua = {
+           --                 diagnostics = {
+           --                     globals = { "vim" }
+           --                 }
+           --             }
+           --         }
+           --     }
+           -- end
+	   }
+	   require("mason-lspconfig").setup_handlers(handlers)
+   end
+  }
 }
