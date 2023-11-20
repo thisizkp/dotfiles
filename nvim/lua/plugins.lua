cfb69ebc20354e8890ad7a6f58ca43e4de77bd03
KP <prasanth.25k@gmail.com>
2023-11-20 13:05:03 +0530

    [nvim] add conform for formatting

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,28 +3,28 @@ return {
 		"ellisonleao/gruvbox.nvim",
 		priority = 1000,
 		config = function()
-			require('gruvbox').setup()
-			vim.cmd('colorscheme gruvbox')
-		end
+			require("gruvbox").setup()
+			vim.cmd("colorscheme gruvbox")
+		end,
 	},
 	{
-		'numToStr/Comment.nvim',
+		"numToStr/Comment.nvim",
 		lazy = false,
 		config = function()
-			require('Comment').setup()
-		end
+			require("Comment").setup()
+		end,
 	},
 	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.4',
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
 		dependencies = {
-			'nvim-lua/plenary.nvim'
+			"nvim-lua/plenary.nvim",
 		},
 		config = function()
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-		end
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		end,
 	},
 	{
 		"nvim-tree/nvim-tree.lua",
@@ -36,15 +36,15 @@ return {
 		config = function()
 			require("nvim-tree").setup({
 				view = {
-					width = 30
-				}
+					width = 30,
+				},
 			})
 		end,
 	},
 	"github/copilot.vim",
 	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
 		lazy = true,
 		config = false,
 		init = function()
@@ -54,65 +54,79 @@ return {
 		end,
 	},
 	{
-		'williamboman/mason.nvim',
+		"williamboman/mason.nvim",
 		lazy = false,
 		config = true,
 	},
 	{
-		'hrsh7th/nvim-cmp',
-		event = 'InsertEnter',
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
 		dependencies = {
-			{ 'L3MON4D3/LuaSnip' },
+			{ "L3MON4D3/LuaSnip" },
 		},
 		config = function()
-			local lsp_zero = require('lsp-zero')
+			local lsp_zero = require("lsp-zero")
 			lsp_zero.extend_cmp()
 
-			local cmp = require('cmp')
+			local cmp = require("cmp")
 			local cmp_action = lsp_zero.cmp_action()
 
 			cmp.setup({
 				formatting = lsp_zero.cmp_format(),
 				mapping = cmp.mapping.preset.insert({
-					['<C-Space>'] = cmp.mapping.complete(),
-					['<C-u>'] = cmp.mapping.scroll_docs(-4),
-					['<C-d>'] = cmp.mapping.scroll_docs(4),
-					['<C-f>'] = cmp_action.luasnip_jump_forward(),
-					['<C-b>'] = cmp_action.luasnip_jump_backward(),
-				})
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
+					["<C-f>"] = cmp_action.luasnip_jump_forward(),
+					["<C-b>"] = cmp_action.luasnip_jump_backward(),
+				}),
 			})
-		end
+		end,
 	},
 	{
-		'neovim/nvim-lspconfig',
-		cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-		event = { 'BufReadPre', 'BufNewFile' },
+		"neovim/nvim-lspconfig",
+		cmd = { "LspInfo", "LspInstall", "LspStart" },
+		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
-			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'williamboman/mason-lspconfig.nvim' },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "williamboman/mason-lspconfig.nvim" },
 		},
 		config = function()
 			-- This is where all the LSP shenanigans will live
-			local lsp_zero = require('lsp-zero')
+			local lsp_zero = require("lsp-zero")
 			lsp_zero.extend_lspconfig()
 
 			lsp_zero.on_attach(function(client, bufnr)
 				-- see :help lsp-zero-keybindings
 				-- to learn the available actions
 				lsp_zero.default_keymaps({ buffer = bufnr })
-				lsp_zero.buffer_autoformat()
 			end)
 
-			require('mason-lspconfig').setup({
-				ensure_installed = {'lua_ls', 'rust_analyzer', 'tailwindcss', 'tsserver', 'vimls', 'zls'},
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "rust_analyzer", "tailwindcss", "tsserver", "vimls", "zls" },
 				handlers = {
 					lsp_zero.default_setup,
 					lua_ls = function()
 						local lua_opts = lsp_zero.nvim_lua_ls()
-						require('lspconfig').lua_ls.setup(lua_opts)
+						require("lspconfig").lua_ls.setup(lua_opts)
 					end,
-				}
+				},
 			})
-		end
-	}
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					javascript = { { "prettierd", "prettier" } },
+				},
+				format_on_save = {
+					timeout_ms = 500,
+					lsp_fallback = true,
+				},
+			})
+		end,
+	},
 }
