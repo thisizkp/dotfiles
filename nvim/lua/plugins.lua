ffdd863efe3068096a1343e66173c67569827cef
KP <prasanth.25k@gmail.com>
2023-11-20 13:35:17 +0530

    [nvim] not happy with stylua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -119,7 +119,6 @@ return {
 		config = function()
 			require("conform").setup({
 				formatters_by_ft = {
-					lua = { "stylua" },
 					javascript = { { "prettierd", "prettier" } },
 				},
 				format_on_save = {
