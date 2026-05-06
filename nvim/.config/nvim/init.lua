1b6d58accbc2b0eed0b21b29b24c6696ed8072ed
KP <prasanth.25k@gmail.com>
2026-05-06 13:03:26 +0530

    Add Neovim theme config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,32 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local is_dark = vim.fn.system("defaults read -g AppleInterfaceStyle 2>/dev/null"):match("Dark") ~= nil
+
+require("lazy").setup({
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    opts = {
+      style = is_dark and "dark" or "light",
+      transparent = true,
+      term_colors = false,
+    },
+    config = function(_, opts)
+      require("onedark").setup(opts)
+      require("onedark").load()
+    end,
+  },
+})
