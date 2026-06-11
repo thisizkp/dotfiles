bf5582741160a7d4e0103b24823cba5e982c97a8
KP <prasanth.25k@gmail.com>
2026-06-11 08:31:19 +0530

    Add Rust LSP hover support

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,6 +17,48 @@ local is_dark = vim.fn.system("defaults read -g AppleInterfaceStyle 2>/dev/null"
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(event)
+    local opts = function(desc)
+      return { buffer = event.buf, desc = desc }
+    end
+
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Show hover documentation"))
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts("Show references"))
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts("Rename symbol"))
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts("Code action"))
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts("Previous diagnostic"))
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts("Next diagnostic"))
+  end,
+})
+
+vim.lsp.config("rust_analyzer", {
+  cmd = { "rust-analyzer" },
+  filetypes = { "rust" },
+  root_markers = { "Cargo.toml", "rust-project.json", ".git" },
+  settings = {
+    ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+      },
+      check = {
+        command = "clippy",
+      },
+    },
+  },
+})
+
+vim.lsp.enable("rust_analyzer")
+
 require("lazy").setup({
   {
     "stevearc/oil.nvim",
